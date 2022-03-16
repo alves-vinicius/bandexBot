bfba046009de0a3e9620832f439bff0ae2aed73c
Vinicius Alves <47111440+alves-vinicius@users.noreply.github.com>
2022-03-16 07:13:49 -0300

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -33,7 +33,7 @@ instanceId=0\n
 page=%2Frucard%2FJsp%2FcardapioSAS.jsp%3Fcodrtn%3D8\n
 scriptSessionId=qEqk7ItaLEzxe*E*86DiBQhKpZl/hKILpZl-dc3rvbwx5`;
     
-    let deferred = Q.defer();
+    let deferred = q.defer();
     axios.post('https://uspdigital.usp.br/rucard/dwr/call/plaincall/CardapioControleDWR.obterCardapioRestUSP.dwr', params)
         .then(response => {
 
