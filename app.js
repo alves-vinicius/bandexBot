1eae1d2083b2f6c762f439b717fc84e102ebae3b
Vinicius Alves <47111440+alves-vinicius@users.noreply.github.com>
2022-03-17 14:27:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.set('view engine', 'ejs');
 
 // index page
 app.get('/', function(req, res) {
-  res.render('bandeco.html');
+  res.render('./bandeco.html');
 });
 
-alert('A');
-const axios = require('axios');
-alert('B');
-const Q = require("q");
+//alert('A');
+//const axios = require('axios');
+//alert('B');
+//const Q = require("q");
 alert('C');
 //const notifications = require('./notifications')
 
@@ -29,39 +29,39 @@ alert('C');
 //winston.add(winston.transports.File, { filename: '../storage/actions.log' })
 //winston.handleExceptions(winston.transports.File, { filename: "../storage/errors.log" });
 
-function fetchMenuFromApi(index) {
-    let params = `callCount=1\n
-windowName=\n
-nextReverseAjaxIndex=0\n
-c0-scriptName=CardapioControleDWR\n
-c0-methodName=obterCardapioRestUSP\n
-c0-id=0\n
-c0-param0=string:${index}\n
-batchId=1\n
-instanceId=0\n
-page=%2Frucard%2FJsp%2FcardapioSAS.jsp%3Fcodrtn%3D8\n
-scriptSessionId=qEqk7ItaLEzxe*E*86DiBQhKpZl/hKILpZl-dc3rvbwx5`;
-    
-    //let deferred = q.defer();
-    axios.post('https://uspdigital.usp.br/rucard/dwr/call/plaincall/CardapioControleDWR.obterCardapioRestUSP.dwr', params)
-        .then(response => {
-
-        let json_doidao = response.data;
-        let json_normal = json_doidao.split('\n').slice(6).join('\n').replace('dwr.engine.remote.handleCallback("1","0",', "").replace("})();", "").trim().replace(");", "")
-        let json_melhor = json_normal.replace(/([\[{,])([a-z][a-z0-9]+):/g, "$1\"$2\":");
-        let cardapio = JSON.parse(json_melhor);
-
-        //deferred.resolve(cardapio);
-    });
-
-    return cardapio;
-    //return deferred.promise;
-}
-
-//SETUP INICIAL
-//console.log('Server up!')
-alert( fetchMenuFromApi( 9));
-cardapios.setupCaching();
-alert('2');
-let menu = cardapios.fetch(splitter[2], splitter[1] == 'ALMOCO' ? 0 : 1);
-alert('3');
+//function fetchMenuFromApi(index) {
+//    let params = `callCount=1\n
+//windowName=\n
+//nextReverseAjaxIndex=0\n
+//c0-scriptName=CardapioControleDWR\n
+//c0-methodName=obterCardapioRestUSP\n
+//c0-id=0\n
+//c0-param0=string:${index}\n
+//batchId=1\n
+//instanceId=0\n
+//page=%2Frucard%2FJsp%2FcardapioSAS.jsp%3Fcodrtn%3D8\n
+//scriptSessionId=qEqk7ItaLEzxe*E*86DiBQhKpZl/hKILpZl-dc3rvbwx5`;
+//    
+//    //let deferred = q.defer();
+//    axios.post('https://uspdigital.usp.br/rucard/dwr/call/plaincall/CardapioControleDWR.obterCardapioRestUSP.dwr', params)
+//        .then(response => {
+//
+//        let json_doidao = response.data;
+//        let json_normal = json_doidao.split('\n').slice(6).join('\n').replace('dwr.engine.remote.handleCallback("1","0",', "").replace("})();", "").trim().replace(");", "")
+//        let json_melhor = json_normal.replace(/([\[{,])([a-z][a-z0-9]+):/g, "$1\"$2\":");
+//        let cardapio = JSON.parse(json_melhor);
+//
+//        //deferred.resolve(cardapio);
+//    });
+//
+//    return cardapio;
+//    //return deferred.promise;
+//}
+//
+////SETUP INICIAL
+////console.log('Server up!')
+//alert( fetchMenuFromApi( 9));
+//cardapios.setupCaching();
+//alert('2');
+//let menu = cardapios.fetch(splitter[2], splitter[1] == 'ALMOCO' ? 0 : 1);
+//alert('3');
